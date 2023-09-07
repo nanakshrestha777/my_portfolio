60430de98b223e6791f19e432f5918cd43d82756
Nanak Shrestha_7 <118968557+nanakshrestha777@users.noreply.github.com>
2023-09-07 08:48:00 +0545

    heroimgadded

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from "../components/Navbar"
+import Heroimg from "../components/Heroimg"
 
 const home = () => {
   return (
     <div>
       <Navbar/>
+      <Heroimg/>
     </div>
   )
 }
