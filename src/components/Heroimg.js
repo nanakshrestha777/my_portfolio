8c03fe971ed626691ee620d12e6e09358d4b2670
Nanak Shrestha_7 <118968557+nanakshrestha777@users.noreply.github.com>
2023-09-07 08:47:16 +0545

    hero section created

---
+++ src/components/Heroimg.js
@@ -0,0 +1,16 @@
+import "./HeroimgStyles.css";
+
+import React from 'react'
+
+const heroimg = () => {
+  return (
+    <div className="hero">
+        <div className="mask">
+            <img></img>
+        </div>
+    </div>
+
+  )
+}
+
+export default heroimg
