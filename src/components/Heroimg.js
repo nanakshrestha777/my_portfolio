1651e7e7c60a3dae197ff03144b0d0d02270279a
Nanak Shrestha_7 <118968557+nanakshrestha777@users.noreply.github.com>
2023-09-11 21:01:55 +0545

    some changes

--- src/components/Heroimg.js
+++ src/components/Heroimg.js
@@ -1,7 +1,7 @@
 import "./HeroimgStyle.css";
 
 import React from "react";
-import IntroImg from "../assets/intro-bg.jpg";
+import IntroImg from "../assets/intro-bg-img.jpg";
 import { Link } from "react-router-dom";
 const heroimg = () => {
   return (
@@ -10,8 +10,8 @@ const heroimg = () => {
         <img className="into-img" src={IntroImg} alt="IntroImg" />
       </div>
       <div className="content">
-        <p>Hello, I'm Nanak Shrestha. </p>
-        <h1>Pursuing Data Science Education</h1>
+        <h1>Hello, I'm Nanak Shrestha. </h1>
+        <p>Pursuing Data Science Education</p>
         <div>
           <button className="rounded-xl border-2 border-blue-500 px-5 py-3 text-base font-medium text-blue-500 transition duration-200 hover:bg-blue-600/5 active:bg-blue-700/5 dark:border-blue-400 dark:bg-blue-400/10 dark:text-white dark:hover:bg-blue-300/10 dark:active:bg-blue-200/10"><Link to="/project" className="btn btn-light">
             Projects
