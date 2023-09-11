540d732164318208ff3453ccffdf79036a833065
Nanak Shrestha_7 <118968557+nanakshrestha777@users.noreply.github.com>
2023-09-11 21:03:19 +0545

    added heading and text

--- src/routes/contact.js
+++ src/routes/contact.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
 import Heroimg from "../components/Heroimg"
+import Heroimg2 from '../components/Heroimg2'
 
 const contact = () => {
   return (
     <div>
       <Navbar/>
+      <Heroimg2 heading="CONTACT." text="Feel free to reach out"/>
       <Footer/>
     </div>
   )
