c39a6df72401f069540fd63fdf590e296ff451d7
Nanak Shrestha_7 <118968557+nanakshrestha777@users.noreply.github.com>
2023-09-11 21:03:28 +0545

    added heading and text

--- src/routes/project.js
+++ src/routes/project.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
-import Heroimg from "../components/Heroimg"
+
+import Heroimg2 from '../components/Heroimg2'
 
 const project = () => {
   return (
     <div>
       <Navbar/>
-     
+      <Heroimg2 heading="PROJECTS." text="Some of my projects."/>
       <Footer/>
 
     </div>
