4186d7750a1cf5ac57708b1c714ea19a395408fe
Nanak Shrestha_7 <118968557+nanakshrestha777@users.noreply.github.com>
2023-09-11 21:03:08 +0545

    added heading and text

--- src/routes/about.js
+++ src/routes/about.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
-import Heroimg from "../components/Heroimg"
+
+import Heroimg2 from '../components/Heroimg2'
 
 const about = () => {
   return (
     <div>
       <Navbar/>
+      <Heroimg2 heading="ABOUT." text="Fetching"/>
       <Footer/>
     </div>
   )
