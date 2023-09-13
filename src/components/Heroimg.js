6e8bb5afe96620f2ff9c956338b01697a7339324
Nanak Shrestha_7 <118968557+nanakshrestha777@users.noreply.github.com>
2023-09-13 21:16:56 +0545

    use of tailwinds and animated buttons

--- src/components/Heroimg.js
+++ src/components/Heroimg.js
@@ -1,7 +1,7 @@
 import "./HeroimgStyle.css";
 
 import React from "react";
-import IntroImg from "../assets/intro-bg-img.jpg";
+import IntroImg from "../assets/img-bg.jpg";
 import { Link } from "react-router-dom";
 const heroimg = () => {
   return (
@@ -12,20 +12,41 @@ const heroimg = () => {
       <div className="content">
         <h1>Hello, I'm Nanak Shrestha. </h1>
         <p>Pursuing Data Science Education</p>
-        <div>
-          <button className="rounded-xl border-2 border-blue-500 px-5 py-3 text-base font-medium text-blue-500 transition duration-200 hover:bg-blue-600/5 active:bg-blue-700/5 dark:border-blue-400 dark:bg-blue-400/10 dark:text-white dark:hover:bg-blue-300/10 dark:active:bg-blue-200/10"><Link to="/project" className="btn btn-light">
-            Projects
-          </Link></button>
-          <button className="rounded-xl border-2 border-blue-500 px-5 py-3 text-base font-medium text-blue-500 transition duration-200 hover:bg-blue-600/5 active:bg-blue-700/5 dark:border-blue-400 dark:bg-blue-400/10 dark:text-white dark:hover:bg-blue-300/10 dark:active:bg-blue-200/10"> <Link to="/contact" className="btn btn-dark">
-            Contact
-          </Link></button>
+        <div className="space-x-6 space-y-3">
+          {/* <button className="rectangle-x border-4 border-purple-800 px-7 py-3 text-base font-medium  text-white hover:bg-black hover:text-purple active:bg-blue-700/5 dark:border-blue-400 dark:bg-blue-400/10 dark:text-white dark:hover:bg-blue-300/10 dark:active:bg-blue-200/10">
+            <Link to="/project" className="btn btn-light">
+              Projects
+            </Link>
 
+          </button> */}
+          <Link to="/project" className="relative inline-flex items-center px-12 py-3 overflow-hidden text-lg font-medium text-blue-600 border-2 border-blue-600 rounded-full hover:text-white group hover:bg-gray-50">
+            <span className="absolute left-0 block w-full h-0 transition-all bg-blue-600 opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
+            <span className="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
+            <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path>
+            </svg>
+            </span>
+            <span className="relative">PROJECTS</span>
+          </Link>
+
+          <Link to="/contact" className="relative inline-flex items-center px-12 py-3 overflow-hidden text-lg font-medium text-pink-600 border-2 border-pink-600 rounded-full hover:text-white group hover:bg-gray-50">
+            <span className="absolute left-0 block w-full h-0 transition-all bg-pink-600 opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
+            <span className="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
+            <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path>
+            </svg>
+            </span>
+            <span className="relative">CONTACT</span>
+          </Link>
+    
           
-          
+     
         </div>
+
       </div>
     </div>
-  );
+      );
 };
 
-export default heroimg;
+      export default heroimg;
+
