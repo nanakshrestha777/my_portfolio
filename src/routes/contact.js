63d24aa58d7d314ba4407ad41dfeae2964dcf2a4
Nanak Shrestha_7 <118968557+nanakshrestha777@users.noreply.github.com>
2023-09-18 20:32:54 +0545

    route

--- src/routes/contact.js
+++ src/routes/contact.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
-import Heroimg from "../components/Heroimg"
+import Conversation from '../components/Conversation'
 import Heroimg2 from '../components/Heroimg2'
 
 const contact = () => {
   return (
     <div>
-      <Navbar/>
-      <Heroimg2 heading="CONTACT." text="Feel free to reach out"/>
-      <Footer/>
+      <Navbar />
+      <Heroimg2 heading="CONTACT." text="Feel free to reach out" />
+      <Conversation />
+      <Footer />
     </div>
   )
 }
