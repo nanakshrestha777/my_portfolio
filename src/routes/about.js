c99824776aed3232a30f5639a91cbea0349c9a95
Nanak Shrestha_7 <118968557+nanakshrestha777@users.noreply.github.com>
2023-09-25 21:19:32 +0545

    added section of about

--- src/routes/about.js
+++ src/routes/about.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
-
+import About from "../components/About"
 import Heroimg2 from '../components/Heroimg2'
 
 const about = () => {
   return (
     <div>
       <Navbar/>
-      <Heroimg2 heading="ABOUT." text="Fetching"/>
+      <Heroimg2 heading="ABOUT." text="On the way to World of Data Science."/>
+      <About/>
+
       <Footer/>
     </div>
   )
