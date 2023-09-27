f33af3b8053ab40c46109ee071477fc29c4dd398
Nanak Shrestha_7 <118968557+nanakshrestha777@users.noreply.github.com>
2023-09-27 21:28:32 +0545

    optimized about section made it responsive

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,7 @@ const About = () => {
     <div className="About">
       <div className="About-container text-2xl text-center py-8">
         <h1>Who am I?</h1>
-        <p>
+        <p className='ms-5'>
           I'm a data science student based at Birmingham City University, passionate about leveraging the power of data and technology to solve real-world problems. I have a strong enthusiasm for learning and continuously exploring new technologies in the ever-evolving field of data science.
         </p>
       </div>
@@ -20,36 +20,37 @@ const About = () => {
         <div className="left-box">
           <h1><span>Experience</span></h1>
           <div className="white-box-left">
-            I have actively sought opportunities to apply my skills in real-world settings. I have collaborated with industry professionals on data science projects, participated in hackathons, and engaged in online communities to stay updated with the latest trends and advancements in the field.
+          "As a dedicated data science student, I am committed to harnessing the power of data and technology to address real-world challenges. I actively engage in research, continuously expand my skill set, and thrive in collaborative team environments to drive innovative solutions in the dynamic field of data science."
           </div>
         </div>
-        <div className="right-box ">
+        <div className="right-box">
           <h1><span>Skills</span></h1>
-          <div className="white-box-right  ms-center">
-            <div className="hover:scale-110 duration-500 my-5">
-              <img className="w-12" src={HTML} alt="HTML icon" />
-              <p className="text">HTML</p>
+          <div className="white-box-right">
+            <div className="skill">
+              <img className="skill-icon" src={HTML} alt="HTML icon" />
+              <p className="skill-name">HTML</p>
             </div>
-            <div className="hover:scale-110 duration-500">
-              <img className="w-10" src={CSS} alt="CSS icon" />
-              <p className="text">CSS</p>
+            <div className="skill">
+              <img className="skill-icon" src={CSS} alt="CSS icon" />
+              <p className="skill-name">CSS</p>
             </div>
-            <div className="hover:scale-110 duration-500">
-              <img className="w-10" src={GITHUB} alt="GitHub icon" />
-              <p className="text">GitHub</p>
+            <div className="skill">
+              <img className="skill-icon" src={GITHUB} alt="GitHub icon" />
+              <p className="skill-name">GitHub</p>
             </div>
-            <div className="hover:scale-110 duration-500">
-              <img className="w-10" src={PYTHON} alt="Python icon" />
-              <p className="text">Python</p>
+            <div className="skill">
+              <img className="skill-icon" src={PYTHON} alt="Python icon" />
+              <p className="skill-name">Python</p>
             </div>
-            <div className="hover:scale-110 duration-500">
-              <img className="w-10" src={FIGMA} alt="Figma icon" />
-              <p className="text">Figma</p>
+            <div className="skill">
+              <img className="skill-icon" src={FIGMA} alt="Figma icon" />
+              <p className="skill-name">Figma</p>
             </div>
-            <div className="hover:scale-110 duration-500">
-              <img className="w-10" src={REACT} alt="React icon" />
-              <p className="text">React</p>
+            <div className="skill">
+              <img className="skill-icon" src={REACT} alt="React icon" />
+              <p className="skill-name">React</p>
             </div>
+         
           </div>
         </div>
       </div>
